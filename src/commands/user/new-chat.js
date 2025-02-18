371491020d79d5c25e97392af08e68c85332a562
Pawarna <pawarnash@gmail.com>
2025-02-18 17:58:17 +0700

    create new-chat command

---
+++ src/commands/user/new-chat.js
@@ -0,0 +1,15 @@
+const { resetChatHistory } = require("../../database/chatHistory.js")
+
+module.exports = {
+    name: 'new-chat',
+    description: 'Mereset percakapan dengan @paww-bot',
+    async execute ({sock, senderId}){
+        try {
+            await resetChatHistory(senderId);
+            
+            await sock.sendMessage(senderId, { text: '🎉 *Sip! Obrolan kita udah Paw reset nih!* 😎 *Yuk, mulai chat baru lagi dengan ketik "paw"!* 😉'})
+        } catch (error) {
+            await sock.sendMessage(senderId, { text: "⚠ *Waduh, ada masalah nih!* 🥺 *Paw gagal reset chat kamu. Coba lagi nanti ya!* 🙏"})
+        }
+    }
+}
